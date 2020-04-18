5f74da04ced9590952f8322000a4ba6b99192c72
Hemant-Arya302 <60865796+Hemant-Arya302@users.noreply.github.com>
2020-04-18 22:01:50 +0530

    Add files via upload

---
+++ helloworldspringboot/src/main/java/com/capg/hem/gla/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.capg.hem.gla;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping(value="/")
+	@ResponseBody
+	public String get() {
+	return "Hello World";
+} 
+}
